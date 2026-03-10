04c06af8b82e3afd20f3a4ca523b95e314c1d162
NuriaSeva <nuriaseva@outlook.es>
2026-03-10 21:09:46 +0100

    Conexion al sandbox de true layer

---
+++ WebApplication1/Models/TrueLayerOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace FindMind.Models;
+
+public class TrueLayerOptions
+{
+    public string ClientId { get; set; } 
+    public string ClientSecret { get; set; } 
+    public string RedirectUri { get; set; }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,19 +1,25 @@
 using FindMind.Data;
 using FindMind.Middleware;
 using Microsoft.EntityFrameworkCore;
+using FindMind.Models;
+using FindMind.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-                      ?? throw new InvalidOperationException("No se encontró la cadena de conexión 'DefaultConnection'.");
+                      ?? throw new InvalidOperationException("No se encontró la cadena de conexión");
 
 builder.Services.AddDbContext<FindMindDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<TrueLayerOptions>(
+    builder.Configuration.GetSection("TrueLayer"));
 
+builder.Services.AddHttpClient<TrueLayerService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
